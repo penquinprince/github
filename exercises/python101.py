07ff88c434474ee8fa908bd07ff53d0e2515a3e8
Daan Veen <d.w.a.vanderveen@student.utwente.nl>
2019-11-18 09:35:35 +0100

    Exercise one, all ok on return

--- exercises/python101.py
+++ exercises/python101.py
@@ -1,6 +1,6 @@
 def foo():
-    """Have this method return True."""
-    pass
+    "Have this method return true"
+    return True
 
 
 def concatenadd(a, b):
@@ -8,22 +8,24 @@ def concatenadd(a, b):
     returns the concatenation of a and b if they contain strings, and the sum
     if they contains ints or floats
     """
-    pass
+    c = a + b
+    return c
+
 
 def cf_color(frogs, clutch):
     cfc = "white"
-    if frogs >= 0 and frogs <= 2:
+    if 0 <= frogs <= 2:
         cfc = "blue"
-    if clutch >= 0:
-        cfc = "green"
-    if clutch >= frogs:
-        cfc = "yellow"
-    elif frogs <= 0 and frogs >= -2:
+        if clutch >= 0:
+            cfc = "green"
+        if clutch >= frogs:
+            cfc = "yellow"
+    elif 0 >= frogs >= -2:
         cfc = "black"
-    if clutch >= 0:
-        cfc = "orange"
-    elif frogs + clutch >= -2:
-        cfc = "red"
+        if clutch >= 0:
+            cfc = "orange"
+        if frogs + clutch >= -2:
+            cfc = "red"
     return cfc
 
 
@@ -34,21 +36,32 @@ def collatz(n):
     to reach 1 (https://oeis.org/A006577)
     """
     counter = 0
-    while n != 1:
-        if n % 2 == 0:
-            n = n // 2
+    if not isinstance(n, int):
+        if isinstance(n, float):
+            n = int(n)
         else:
-            n = 3 * n + 1
-    counter += 1
-    return counter
-
+            print("Input is not a number")
+            return None
+    if n >= 1:
+        while n != 1:
+            if n % 2 == 0:
+                n = n // 2
+            else:
+                n = 3 * n + 1
+            counter += 1
+        return counter
+    else:
+        print("Value input too low")
+        return None
 
 
 def even_steven(word):
     """This function returns a string with every second letter of
     the word, starting at the second letter."""
     new_word = ""
-    pass
+    for i in range(0, len(word)):
+        new_word.append(word[i+1])
+        i += 2
     return new_word
 
 
@@ -56,13 +69,39 @@ def extend_e(word):
     """This function inserts the letter 'e' after each vowel in word."""
     vowels = "aeiouyAEIOUY"
     new_word = ""
-    pass
+    for i in range(0, len(word)):
+        if word[i] in vowels:
+            new_word = new_word + word[i]
+            new_word = new_word + "e"
+        else:
+            new_word = new_word + word[i]
+
+    return new_word
 
 
 def reply2argument(argument):
     """Given an input string it will return an argument."""
+    words = argument.split()
     reply = ""
-    pass
+    if words[0] == 'Yes,':
+        reply = 'No! '
+    if words[0] == 'No':
+        reply = 'Yes! '
+
+    if argument[-1:] == '?':
+        reply += 'None of your business. '
+    elif 'I' in words:
+        reply += "No, you aren't."
+    elif 'you' in words or 'You' in words:
+        reply += "No, I'm not. "
+    elif 'It' in words or 'it' in words:
+        reply += "No, it isn't."
+    elif "this" in words or "This" in words:
+        reply += "No, not true. "
+    else:
+        reply += "Ooooh! "
+        reply += extend_e(argument)
+
     return reply
 
 
